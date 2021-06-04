82cf96325d7609a7d4844d072b83265db2ce52d5
Pradeepa K P <69074058+pradeepkp625@users.noreply.github.com>
2021-06-04 21:06:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-const click= document.querySelector(".menu span");
-click.addEventListener("click",function(){
-    var x = document.querySelector(".menu__overlay");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-})
+
+function myFunction() {
+
+  var x = document.querySelector(".menu__overlay");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
+  else {
+    x.style.display = "block";
+  } 
+}
